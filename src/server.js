46114ead33d06cf07acc968f4b9d3455b877b6fe
Alejandro <jeffersonaleandro27@gmail.com>
2021-06-07 18:43:32 -0500

    Add MONGO_URU

--- src/server.js
+++ src/server.js
@@ -50,7 +50,7 @@ app.use(require('./rutas'));
 app.use(express.static(path.resolve(__dirname, './public')));
 
 //DB
-mg.connect('mongodb://localhost:27017/divina', { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true }, (err, res) => {
+mg.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true }, (err, res) => {
   if (err) throw err;
   console.log('Data base is alive in port ', 3000);
 });
